9e959b46a74a38992a3d7e5bc63caa03d484d94b
Bogdan <protsenkobogdan@gmail.com>
2019-05-15 01:24:32 +0200

    btn change

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -6,7 +6,6 @@ import Button from "react-bootstrap/Button";
 const MyButton = () => (
     <Row>
         <Col sm={8}>
-           <div className="fab fa-500px"></div>
             <Button variant="danger" className='orange-btn' size="lg" block>
                 Start booking
             </Button>
